facc623814424005ff18b748cb2580fd3c25fc50
Itzik392 <itzik392@gmail.com>
2023-06-13 17:31:43 +0300

    update

--- function redirectToNewPage(event) {.js
+++
@@ -1,8 +0,0 @@
-function redirectToNewPage(event) {
-    event.preventDefault(); // מניעת שליחת הטופס
-  
-    // כאן ניתן לבצע פעולות נוספות לפני הניתוב לדף החדש
-  
-    window.location.href = "דף_חדש.html"; // הניתוב לדף החדש
-  }
-  
